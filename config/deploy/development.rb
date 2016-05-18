a3ad514211c222addfd710180978bf11bd38c5b3
luxueyan <luxueyan@kaitongamc.com>
2016-05-18 11:34:08 +0800

    update development branch

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -4,5 +4,5 @@ set :user, 'deploy' # Username in the server to SSH to.
 set :port, '22' # SSH port number.
 set :link_backend_assets, '/home/deploy/apps/lode-backend/current/static/assets'
 set :shared_uploads, '/home/deploy/uploads'
-set :branch, 'test'
+set :branch, 'feature/analytics'
 set :common_branch, 'test'
