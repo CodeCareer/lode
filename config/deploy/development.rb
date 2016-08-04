bb81909a250f854a878bbe3959c088654c122590
luxueyan <luxueyan@kaitongamc.com>
2016-08-04 11:42:49 +0800

    update deploy branch

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -4,5 +4,5 @@ set :user, 'deploy' # Username in the server to SSH to.
 set :port, '22' # SSH port number.
 set :link_backend_assets, '/home/deploy/apps/lode-backend/current/static/assets'
 set :shared_uploads, '/home/deploy/uploads'
-set :branch, 'feature/analytics'
+set :branch, 'test'
 set :common_branch, 'test'
--- config/deploy/production02.rb
+++ config/deploy/production02.rb
@@ -4,5 +4,5 @@ set :user, 'deploy'    # Username in the server to SSH to.
 set :port, '10080'     # SSH port number.
 set :link_backend_assets, '/home/deploy/apps/lode-backend/current/static/assets'
 set :shared_uploads, '/home/deploy/uploads'
-set :branch, 'feature/analytics'
+set :branch, 'master'
 set :common_branch, 'master'
